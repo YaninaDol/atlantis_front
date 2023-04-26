4cd72f5a35f150f79bfc311df3e7c878449b0b1b
Yana <95874369+YaninaDol@users.noreply.github.com>
2023-04-27 00:24:14 +0400

    add video

--- src/News.js
+++ src/News.js
@@ -85,21 +85,7 @@ export default function News(){
 
             <Carousel  >
 
-            <Carousel.Item>
-      <video
-      width={1490}
-      height={950}
-            className="slider-video "
-            src="https://www.atlantis.com/-/media/atlantis/dubai/atr/atlantis-the-royal/videos/grand-reveal/atr-grandreveal-herobanner.mp4"
-            loop
-            autoPlay
-            muted
-            loading="lazy"
-          ></video>
-      </Carousel.Item>
-
-
-
+            
       <Carousel.Item active>
         <img
         height={900}
@@ -131,13 +117,64 @@ export default function News(){
         />
 
       </Carousel.Item>
-   
+      <Carousel.Item>
+      <video
+      width={1490}
+      height={950}
+            className="slider-video "
+            src="https://www.atlantis.com/-/media/atlantis/dubai/atr/atlantis-the-royal/videos/grand-reveal/atr-grandreveal-herobanner.mp4"
+            loop
+            autoPlay
+            muted
+            loading="lazy"
+          ></video>
+      </Carousel.Item>
 
     </Carousel>
 
 
+    <div style={{marginTop:100}} className='mainheader'>
+    <h2 class="mainTitle">THIS IS DUBAI’S NEWEST ICON</h2>
+    <span className='titleUnderline'></span>
+    <div class="mainTitleText">
+    Atlantis The Royal set the stage for its Grand Reveal Weekend with an exclusive performance by global icon, Beyoncé. The resort unveiled itself to the world with a three-day star-studded weekend (20th-22nd January, 2023), where global celebrities and special guests from around the world came to celebrate the most ultra-luxury experiential resort in the world.
+
+</div>
+</div>
 
 
+<div>
+<video
+      width={1490}
+      height={950}
+            className="slider-video "
+            src="https://www.atlantis.com/-/media/atlantis/dubai/atr/atlantis-the-royal/videos/grand-reveal/grand-reveal-full-recap_lr.mp4"
+            loop
+            autoPlay
+            muted
+            loading="lazy"
+          ></video>
+</div>
+
+
+
+<div style={{marginTop:100}} className='mainheader'>
+    <h2 class="mainTitle">THIS IS PERFECTION PUTTING ON A SHOW</h2>
+    <span className='titleUnderline'></span>
+    <div class="mainTitleText">
+    Beyoncé lit up the stage after a 5-year hiatus with an awe-inspiring performance, accompanied by Firdaus, 
+    a 48-person orchestra. From the powerful choreography by Emmy-nominated Fatima Robinson, to the featured 
+    Mayyas dance group and a special duet with her daughter Blue Ivy, Beyoncé’s performance was unforgettable.
+     The song line-up included a soulful rendition of Etta James's "At Last," before diving into a medley of 
+     her greatest hits including "XO," "Halo," "Beautiful Liar," "Crazy in Love," "Naughty Girl," and "Drunk in Love".
+     <br/>
+     <br/>
+     In a breathtaking finale moment, Beyoncé was raised 16 feet into the air as a first-of-its kind fireworks display lit up the night sky using drones created by Grucci. To end the evening, guests celebrated into the morning hours with a live afterparty performance by the international DJ supergroup Swedish House Mafia.
+      A spectacle unlike anything the world has ever seen, Atlantis The Royal has made history.
+      <br/>
+    This Is It.
+</div>
+</div>
 
 
         </div>
